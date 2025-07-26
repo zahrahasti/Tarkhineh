0672a05418a25da2f38d563d67d32f8c5c971472
zahrahasti <hastizahra984@gmail.com>
2025-07-26 16:32:30 +0330

    feat(button): add BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    bgButton?: string
+    hasBorder?: string
+    hasHeight?: 'h-[32px]' | 'h-[40px]'
+    width?: string
+  }>(),
+  {
+    bgButton: '',
+    hasBorder: '',
+    hasHeight: 'h-[32px]',
+    width: 'w-full',
+  },
+)
+</script>
+
+<template>
+  <button
+    class="h-[32px] lg:h-[40px] w-inherite flex items-center justify-center gap-1 p-3xs rounded-sm"
+    :class="[props.bgButton, props.hasHeight, props.hasBorder, props.width]"
+  >
+    <slot />
+  </button>
+</template>
