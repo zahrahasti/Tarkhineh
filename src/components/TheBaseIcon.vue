fc29f5d911dcdf51efadc540be4ef51ed0514bf5
zahrahasti <hastizahra984@gmail.com>
2025-07-26 16:38:24 +0330

    feat(icon): add TheBaseIcon component

---
+++ src/components/TheBaseIcon.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  color?: {
+    type: string
+    default: '#FFF'
+  }
+  name: string
+}>()
+</script>
+
+<template>
+  <div class="w-4 sm:w-6">
+    <VsxIcon :icon-name="props.name" :size="undefined" :color="props.color" type="linear" />
+  </div>
+</template>
